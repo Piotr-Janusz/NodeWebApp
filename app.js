d0caa5967fdcab43437cb3c73e292bc008e5c907
Nojus Visneveckas <psynv4@exmail.nottingham.ac.uk>
2022-12-14 12:54:26 +0000

    link js libraries

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+const path = require('path');
+app.use( express.static( __dirname + '/public' ));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/html/index.html'));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
